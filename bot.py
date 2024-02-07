3377cd4b10b5c5727f8b2422c40fdf23ab938df0
NAMAROy <96283395+NAMAROy@users.noreply.github.com>
2024-02-07 13:56:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,22 +36,22 @@ from plugins import web_server
 
 import asyncio
 from pyrogram import idle
-from lazybot import LazyPrincessBot
+from SMKBOTZ import SMKTHEBOTZ
 from util.keepalive import ping_server
-from lazybot.clients import initialize_clients
+from SMKTHEBOTZ.clients import initialize_clients
 
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
-LazyPrincessBot.start()
+SMKTHEBOTZ.start()
 loop = asyncio.get_event_loop()
 
 
 async def Lazy_start():
     print('\n')
     print('Initalizing The Movie Provider Bot')
-    bot_info = await LazyPrincessBot.get_me()
-    LazyPrincessBot.username = bot_info.username
+    bot_info = await SMKTHEBOTZ.get_me()
+    SMKTHEBOTZ.username = bot_info.username
     await initialize_clients()
     for name in files:
         with open(name) as a:
@@ -70,11 +70,11 @@ async def Lazy_start():
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
     await Media.ensure_indexes()
-    me = await LazyPrincessBot.get_me()
+    me = await SMKTHEBOTZ.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
-    LazyPrincessBot.username = '@' + me.username
+    SMKTHEBOTZ.username = '@' + me.username
     logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
     logging.info(LOG_STR)
     logging.info(script.LOGO)
@@ -82,7 +82,7 @@ async def Lazy_start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await LazyPrincessBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    await SMKTHEBOTZ.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
